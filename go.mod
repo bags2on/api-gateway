680c8e7cff725566b9bcdf572c2be7e38db6275e
Vlad-Shevliakov <vladislavs319@gmail.com>
2020-04-18 02:18:09 +0300

    set initial schema

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/bags2on/api-gateway
+
+go 1.13
+
+require (
+	github.com/99designs/gqlgen v0.11.3
+	github.com/vektah/gqlparser/v2 v2.0.1
+)
---
+++ graph/schema.resolvers.go
@@ -0,0 +1,21 @@
+package graph
+
+// This file will be automatically regenerated based on the schema, any resolver implementations
+// will be copied through when generating and any unknown code will be moved to the end.
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/bags2on/api-gatway/graph/generated"
+	"github.com/bags2on/api-gatway/graph/model"
+)
+
+func (r *queryResolver) Products(ctx context.Context) ([]*model.Product, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
+// Query returns generated.QueryResolver implementation.
+func (r *Resolver) Query() generated.QueryResolver { return &queryResolver{r} }
+
+type queryResolver struct{ *Resolver }
